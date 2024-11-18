fc576c7d7b00d885e5c51c4564adbcf76c559644
30121minsik <22167@dongdaemun.ms.kr>
2024-11-18 16:28:50 +0900

    Create 241118.py

---
+++ 241118.py
@@ -0,0 +1,42 @@
+
+
+import tkinter as t
+window = None
+display_label = None
+expression = ''
+
+def press(num):
+    global expression 
+    expression += str(num)
+    display_label['text'] = expression
+
+def press_clear():
+        global expression
+        expression =''
+        display_label['text'] = '0'
+        
+def press_result():
+        global expression
+        expression = ''
+        display_label['text'] = str(eval(expression))
+        expression =''
+def setup_GUI():
+            global window
+            global display_label
+
+            window = t.Tk()
+            window.title('my calc')
+            display_label = t.Label(window, text='', anchor='e', relief=t.SUNKEN, width=15, font='Arial 20')
+            display_label.grid(row=0, columnspan=4)
+            
+            btn1 = t.Button(window, text='1' , width=5, height=2, font='Aral 15', command=lambda:press(1))
+            clear_btn = t.Button(window, text='c', width=5, height=2, font='Arial 15', command=press_clear)
+            result_btn = t.Button(window, bg='green', text='=', width=5, height=2, font='Arial 15', command=press_clear)
+            add_btn = t.Button(window, text='+' , width=5, height=2 , font='Arial 15' ,command=press_result)
+
+            btn1.grid(row=1, column=0)
+            clear_btn.grid(row=4, column=0)
+            result_btn.grid(row=4, column=2) 
+            add_btn.grid(row=1, column=3)                                              
+            
+setup_GUI()
